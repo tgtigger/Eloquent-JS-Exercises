3de6a1cdccba1082db8bc41f210a294941833800
Antonio Gutierrez <tgutierrez77@gmail.com>
2021-05-08 03:50:28 -0400

    WIP Exercise 2.1
    start of work on Exercise 2.1 in Eloquent JS

---
+++ Eloquent Javascript/Chapter2/exercise_2.1.js
@@ -0,0 +1,21 @@
+/*
+**Looping a triangle
+
+Write a loop that makes seven calls to console.log to output the following triangle:
+
+#
+##
+###
+####
+#####
+######
+#######
+
+It may be useful to know that you can find the length of a string by writing .length after it.
+
+let abc = "abc";
+console.log(abc.length);
+// → 3
+
+Most exercises contain a piece of code that you can modify to solve the exercise. Remember that you can click code blocks to edit them.
+*/
